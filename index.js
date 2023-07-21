bc4226c1940a7ec6dd28cd655dbe44273d1a7c51
Gayle Hoefer <gaylehoefer@gmail.com>
2023-07-20 21:29:05 -0700

    inital setuo

---
+++ index.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <canvas id="canvas" width="500" height ="500"></canvas>
+    <div style ="display: none">
+        <img id="animations" src ="assets/animations.gif" alt=""></img>
+
+    </div>
+</body>
+</html>
